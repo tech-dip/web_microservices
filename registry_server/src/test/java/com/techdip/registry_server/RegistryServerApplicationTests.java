84b4a6abf7206a5742570ecfe62f4bcba6cc81eb
Deepak_oodles <deepak.chauhan@oodlestechnologies.com>
2020-10-01 11:33:09 +0530

    registry server  added

---
+++ registry_server/src/test/java/com/techdip/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdip.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
